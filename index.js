aa46d7e14612ff243331fc707b9b86cd236f0d90
Abubakar1010 <abubakar850772@gmail.com>
2024-02-16 16:34:06 +0600

    add discount coupon validation

--- index.js
+++ index.js
@@ -12,8 +12,6 @@ for ( let i = 0; i < cards.length; i++){
 
     const card = cards[i]
 
-    
-
     card.addEventListener('click', function(){
 
         const title =card.querySelector("h3").innerText
@@ -33,8 +31,48 @@ for ( let i = 0; i < cards.length; i++){
 
         totalPrice += price
 
-        console.log(totalPrice);
-
         document.getElementById('totalPrice').innerText = totalPrice
+
+        //* check is coupon are available for user
+
+        if( totalPrice >= 200){
+            isCouponValid("input-field")
+        }
     })
 }
+
+// discount validation
+
+function isCouponValid(elementId){
+
+    const target = document.getElementById(elementId)
+
+    target.removeAttribute('disabled')
+}
+
+const discountButton = document.getElementById("apply-btn")
+
+discountButton.addEventListener('click', function(){
+
+    const inputField = document.getElementById('input-field')
+
+    const userCode = inputField.value.toUpperCase();
+
+    if( userCode === "SALE20"){
+
+        const discountNumber = (totalPrice / 100) * 20
+
+        const yourDiscount = discountNumber.toFixed(2)
+
+        const parent = document.getElementById('discountPrice')
+
+        parent.innerText = yourDiscount
+
+        const finalAmount = document.getElementById('total')
+
+        finalAmount.innerText = totalPrice - yourDiscount
+
+    }
+
+
+})
